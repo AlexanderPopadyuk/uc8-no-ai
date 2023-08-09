0736b93de5f1f057cd0cb81b93b2539d581247a5
Oleksandr Popadiuk <opopa@softserveinc.com>
2023-08-09 21:18:28 +0300

    UC#8 Display values

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,17 +1,31 @@
 import { Form } from '../components/Form';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { actions as formActions } from '../store/form/actions';
 
 export const FormContainer = () => {
   const dispatch = useDispatch();
+  const formState = useSelector(store => store.form);
+  const formIsCompleted = Object.values(formState).some(value => !!value);
 
   const onSubmit = values => {
     dispatch({ type: formActions.update, payload: values });
   };
 
+  const onClear = () => {
+    dispatch({ type: formActions.clear });
+  };
+
   return (
     <div className="form-container">
-      <Form onSubmit={onSubmit} />
+      {!formIsCompleted && <Form onSubmit={onSubmit} />}
+      {formIsCompleted && (
+        <div className="form-results">
+          {Object.entries(formState).map(([field, value]) => (
+            <div key={field}>{field}: {value}</div>
+          ))}
+          <button className="main-button" onClick={onClear}>Clear state</button>
+        </div>
+      )}
     </div>
   );
 };
